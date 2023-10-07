663c4915f52dffc16a270bd69b8f416600395efe
ElordeCatabay <elgu.catabay.up@phinmaed.com>
2023-10-07 18:46:38 +0800

    first commit

---
+++ constants/index.js
@@ -0,0 +1,38 @@
+export const categories = [
+  'All',
+  'Popular',
+  'Recommended',
+  'Programming',
+  'Web Development',
+  'Database',
+  'Animation',
+  'System Development'
+
+]
+
+export const foodItems = [
+  {
+    name: 'ITE232',
+    ingredients: 'Subject code',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.',
+    image: require('../assets/images/subject.jpg')
+  },
+  {
+    name: 'ITE232',
+    ingredients: 'Subject code',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.',
+    image: require('../assets/images/subject.jpg')
+  },
+  {
+   name: 'ITE232',
+    ingredients: 'Subject code',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.',
+    image: require('../assets/images/subject.jpg')
+  },
+  {
+    name: 'ITE232',
+    ingredients: 'Subject code',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.',
+    image: require('../assets/images/subject.jpg')
+  },
+]
